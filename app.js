f394bfaab22e8a0d1294face0530baef6bb88705
Nolen Lawton <mnlawton@icloud.com>
2023-01-21 15:39:27 -0600

    added edit and cancel buttons

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 $(document).ready(onReady)
 
-let employeesArray = []
+let employeesArray = [
+    {
+        firstName: "Nolen",
+        lastName: "Lawton",
+        id: 8,
+        jobTitle: "Hobo",
+        annualSalary: 240000,
+      },
+]
+
+let employeeIndex = undefined;
 
 function onReady() {
     $('#application').on('submit', addEmployee)
 
     $(document).on('click', '.deleteButton', deleteEmployee)
+
+    $(document).on('click', '.editButton', editEmployee)
+
+    $(document).on('click', '.cancelButton', cancelEditedEmployee)
+
+    $(document).on('click', '.addButton', addEditedEmployee)
 
     render()
 }
@@ -29,12 +45,39 @@ function addEmployee(event) {
 }
 
 function deleteEmployee() {
-    console.log('hello')
     let employeeIndex = $(this).parent().parent().index();
-    console.log(employeeIndex)
     employeesArray.splice(employeeIndex, 1)
 
     render()
+}
+
+function editEmployee() {
+    employeeIndex = $(this).parent().parent().index();
+    console.log(employeeIndex)
+    render()
+}
+
+function cancelEditedEmployee(){
+    employeeIndex = undefined;
+    render()
+}
+
+function addEditedEmployee(){
+    console.log('add')
+
+    let employee = {
+        firstName: $('#editFirstName').val(),
+        lastName: $('#editLastName').val(),
+        id: $('#editId').val(),
+        jobTitle: $('#editJobTitle').val(),
+        annualSalary: $('#editAnnualSalary').val()
+    }
+    employeesArray.push(employee);
+
+    totalMonthly();
+    render();
+
+    $('#application input').val('')
 }
 
 function totalMonthly() {
@@ -60,19 +103,40 @@ function render() {
     $('#employee-table').empty()
     $('#totalMonthly').empty()
 
+
     for(let employee of employeesArray) {
-        $('#employee-table').append(`
-        <tr>
-            <td>${employee.firstName}</td>
-            <td>${employee.lastName}</td>
-            <td>${employee.id}</td>
-            <td>${employee.jobTitle}</td>
-            <td>${employee.annualSalary}</td>
-            <td><button class='editButton'>Edit</button></td>
-            <td><button class='deleteButton'>Delete</button></td>
-        </tr>
-        `)
-    }
+
+        if(employeesArray.indexOf(employee) === employeeIndex) {
+
+            $('#employee-table').append(`
+                <tr>
+                    <td><input id='editFirstName' type='text' value='${employeesArray[employeeIndex].firstName}'/></td>
+                    <td><input id='editLastName' type='text' value='${employeesArray[employeeIndex].lastName}'/></td>
+                    <td><input id='editId' type='number' value='${employeesArray[employeeIndex].id}'/></td>
+                    <td><input id='editJobTitle' type='text' value='${employeesArray[employeeIndex].jobTitle}'/></td>
+                    <td><input id='editAnnualSalary' type='number' value='${employeesArray[employeeIndex].annualSalary}'/></td>
+                    <td><button class='cancelButton'>Cancel</button></td>
+                    <td><button class='addButton'>Add</button></td>
+                </tr>
+            `)
+        }
+
+        else {
+
+
+            $('#employee-table').append(`
+            <tr>
+                <td>${employee.firstName}</td>
+                <td>${employee.lastName}</td>
+                <td>${employee.id}</td>
+                <td>${employee.jobTitle}</td>
+                <td>${employee.annualSalary}</td>
+                <td><button class='editButton'>Edit</button></td>
+                <td><button class='deleteButton'>Delete</button></td>
+            </tr>
+            `)
+        }
+    }  
 
     $('#totalMonthly').append(`
         <h2>Total Monthly: $ ${totalMonthly()}
